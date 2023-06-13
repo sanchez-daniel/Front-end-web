0635e4a27b39a17adaaa452a757ec3f6fdcb9f67
sanchez-daniel <daniel.asanchezm@unac.edu.co>
2023-06-13 17:06:25 -0500

    imagenes

---
+++ src/components/crear.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom'
+import { createTask } from '../lib/pocketbase';
+import { useNavigate } from 'react-router-dom';
+
+function Create() {
+
+  const [name, setname] = useState(null);
+  const [color, setcolor] = useState(null);
+  const [estado, setestado] = useState(null);
+  const [Modelo, setModelo] = useState(null);
+  const [Marca, setMarca] = useState(null);
+  const [Linea, setLinea] = useState(null);
+  const [descripcion, setdecripcion] = useState(null);
+  
+
+  const handleSubmit = ()=>{
+    if(!title){
+      window.alert("submit the title please");
+      return
+    }
+    createTask(title, des
--- src/pages/Mantenimiento.jsx
+++ src/pages/Mantenimiento.jsx
@@ -5,12 +5,20 @@ import { Table } from "../components/Table";
 export function Mantenimiento() {
   return (
     <>
-      
-      <SideBar/>
-      <button></button>
-      <button></button>
-      <button></button>
-      <Table/>
+      <SideBar />
+      <div className="my-3">
+        <button className="my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Ingresar
+        </button>
+        <button className=" my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Eliminar
+        </button>
+        <button className="my-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          actualizar
+        </button>
+      </div>
+
+      <Table />
     </>
   );
 }
